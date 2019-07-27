f482d978695b89bb197d60bd36c63ed6ec7d67ed
Abhinav KM <abhinavkm184@gmail.com>
2019-07-27 19:18:23 +0530

    node added

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+
+//add the router
+app.use(express.static(__dirname + '/static'));
+//Store all JS and CSS in Scripts folder.
+
+app.use('/', router);
+app.listen(3000);
+
+console.log('Running at Port 3000');
